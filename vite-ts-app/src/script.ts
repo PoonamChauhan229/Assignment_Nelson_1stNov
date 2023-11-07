816b45adb51f2e43cc09f526927dfb49b3891c4f
Poonam229 <poonam.chauhan229@gmail.com>
2023-11-07 17:56:02 +0530

    project searchbar working

---
+++ vite-ts-app/src/script.ts
@@ -0,0 +1,5 @@
+const data = loadJSON('../DO_NOT_TOUCH/data.json'); //Don't delete this line. All your data is here. It does take a few seconds for Replit to load the data because it's so large.
+console.log (data)
+
+
+
