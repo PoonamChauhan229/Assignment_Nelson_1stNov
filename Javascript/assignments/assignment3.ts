8015c1b8bc9cb2c56f78f05c339d80cc6accefb0
Poonam229 <poonam.chauhan229@gmail.com>
2023-12-22 06:38:17 +0530

    usage

--- Javascript/assignments/assignment3.ts
+++ Javascript/assignments/assignment3.ts
@@ -34,3 +34,28 @@ class Platform {
     }
   }
   
+  // Create instances of the Video class
+const video1 = new Video('Action Movie', 120, 'movie', ['YouTube']);
+const video2 = new Video('Comedy Show', 30, 'show', ['Netflix']);
+
+// Create an instance of the Platform class
+const netflix = new Platform('Netflix');
+
+// Add videos to the platform using the addVideo method
+netflix.addVideo(video1);
+netflix.addVideo(video2);
+
+// List videos on the platform
+netflix.listVideos(); // Output: Videos on Netflix platform:\nAction Movie\nComedy Show
+
+// Create a new video on the platform using the createVideo method
+netflix.createVideo('Drama Movie', 150, 'movie');
+
+// List videos again to see the updated list
+netflix.listVideos(); // Output: Videos on Netflix platform:\nAction Movie\nComedy Show\nDrama Movie
+
+// Remove a video from the platform using the removeVideo method
+netflix.removeVideo('Comedy Show');
+
+// List videos again to see the updated list
+netflix.listVideos(); // Output: Videos on Netflix platform:\nAction Movie\nDrama Movie
